b4918f4b929a579e5f88a2b7e1b82947bedc12f2
Vadim Chaika <proxik777@gmail.com>
2020-01-14 21:29:50 +0200

    LOGGER

--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -5,13 +5,17 @@ import javax.servlet.ServletContextListener;
 
 import mate.academy.internetshop.lib.Injector;
 
+import org.apache.log4j.Logger;
+
 public class InjectInitializer implements ServletContextListener {
+    public static final Logger LOGGER = Logger.getLogger(Injector.class);
+
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
-            System.out.println("Dependency injection started");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
+            LOGGER.error("Dependency injection has not completed");
             throw new RuntimeException(e);
         }
     }
